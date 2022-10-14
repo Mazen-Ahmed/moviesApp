b3c9e930d8137d40c143a7a7a1e06ccde4448def
Mazen Ahmed <mazenreal22@gmail.com>
2022-10-14 20:27:39 +0200

    Updates

---
+++ static/js/255.a2a0c4b4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoviesapp=self.webpackChunkmoviesapp||[]).push([[255],{1255:function(e,n,t){t.r(n);t(2791);var i=t(1087),r=t(6015),s=t(4565),c=t(184);n.default=function(){return(0,c.jsxs)(r.Z,{sx:{height:"100vh",width:"100%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"},children:[(0,c.jsx)(s.Z,{variant:"h4",children:"404 not found"}),(0,c.jsx)(s.Z,{variant:"h6",children:(0,c.jsx)(i.rU,{to:"/",style:{textDecoration:"underline",color:"#000"},children:"Back To Home"})})]})}}}]);
+//# sourceMappingURL=255.a2a0c4b4.chunk.js.map
